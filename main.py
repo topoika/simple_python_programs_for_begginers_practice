5c6eb9f32da38e9ade32882e7d9472397d0ab97b
topoika <topoikatampul@gmail.com>
2021-02-02 20:09:22 +0300

    new commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from _csv import writer
+
 import paramiters
 # paramiters.find_the_max_value()
 # paramiters.find_frequency_of_an_element_in_an_array()
@@ -6,4 +8,12 @@ import paramiters
 # paramiters.get_the_sum_of_multiple_parameters()
 # paramiters.fint_the_squres_of_even_numbers()
 
-paramiters.grading_system()
+# paramiters.grading_system()
+
+
+while True:
+    ourFile = open('myname.txt', 'w+')
+    name = input("Name: ")
+    age = int(input("Age: "))
+ourFile.write(f"Name: {name}\n")
+ourFile.write(f"Age: {age}\n")
